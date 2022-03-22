5c98af262915ae1799de1bb881ace6eb67572889
Alexander Staeding <alexanderstaeding@gmail.com>
2022-03-22 17:03:48 +0100

    Initial Commit

---
+++ build.gradle.kts
@@ -0,0 +1,57 @@
+import org.sourcegrade.submitter.submit
+
+plugins {
+    java
+    application
+    id("org.sourcegrade.submitter").version("0.4.0")
+}
+
+submit {
+    assignmentId = "h_id_" // do not change assignmentId
+    studentId = null // TU-ID  z.B. "ab12cdef"
+    firstName = null
+    lastName = null
+    // Optionally require tests for prepareSubmission task. Default is true
+    requireTests = true
+}
+
+// !! Achtung !!
+// Die studentId (TU-ID) ist keine Matrikelnummer
+// Richtig z.B. ab12cdef
+// Falsch z.B. 1234567
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains:annotations:23.0.0")
+    // JUnit only available in "test" source set (./src/test)
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
+}
+
+application {
+    mainClass.set("h_id_.Main")
+}
+
+tasks {
+    val runDir = File("build/run")
+    named<JavaExec>("run") {
+        doFirst {
+            runDir.mkdirs()
+        }
+        workingDir = runDir
+    }
+    test {
+        doFirst {
+            runDir.mkdirs()
+        }
+        workingDir = runDir
+        useJUnitPlatform()
+    }
+    withType<JavaCompile> {
+        options.encoding = "UTF-8"
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
+    }
+}
