892c79b43fa91259b31f3a0edadf25d655ff675d
Dustin Glaser <dustin.glaser@posteo.de>
2022-08-15 15:39:39 +0200

    add template

---
+++ src/main/java/h01/Checkers.java
@@ -0,0 +1,134 @@
+package h01;
+
+import fopbot.Robot;
+import fopbot.World;
+
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+import static org.tudalgo.algoutils.student.Student.crash;
+import static org.tudalgo.algoutils.student.io.PropertyUtils.getIntProperty;
+
+public class Checkers {
+
+    /**
+     * The number of rows in the game board.
+     */
+    public static final int NUMBER_OF_ROWS = getIntProperty("checkers.properties", "NUMBER_OF_ROWS");
+
+    /**
+     * The number of columns in the game board.
+     */
+    public static final int NUMBER_OF_COLUMNS = getIntProperty("checkers.properties", "NUMBER_OF_COLUMNS");
+
+    /**
+     * The minimum initial number of coins for a black stone.
+     */
+    public static final int MIN_NUMBER_OF_COINS = getIntProperty("checkers.properties", "MIN_NUMBER_OF_COINS");
+
+    /**
+     * The maximum initial number of coins for a black stone.
+     */
+    public static final int MAX_NUMBER_OF_COINS = getIntProperty("checkers.properties", "MAX_NUMBER_OF_COINS");
+
+    /**
+     * The current state of the game.
+     * At the start of the game, the state of the game is set to {@link GameState#RUNNING}.
+     * After the game has finished, the state of the game is set to {@link GameState#BLACK_WIN} or {@link GameState#WHITE_WIN}.
+     */
+    private GameState gameState = GameState.RUNNING;
+
+
+    /**
+     * The robot of the white team.
+     */
+    private Robot whiteStone;
+
+    /**
+     * The robots of the black team.
+     */
+    private Robot blackStone0, blackStone1, blackStone2, blackStone3, blackStone4;
+
+    public static void main(String[] args) {
+        Checkers checkers = new Checkers();
+        checkers.initGame();
+        checkers.runGame();
+    }
+
+    /**
+     * Runs the initialization of the game.
+     * The initialization of the game consists of the initialization of the world and all stones.
+     */
+    public void initGame() {
+        // initialize the world
+        World.setSize(NUMBER_OF_COLUMNS, NUMBER_OF_ROWS);
+        // initialize all stones
+        initWhiteStone();
+        initBlackStones();
+    }
+
+    /**
+     * Runs the game. After the game has finished, the winner of the game will be printed to the console.
+     */
+    public void runGame() {
+        World.setVisible(true);
+        while (isRunning()) {
+            doBlackTeamAction();
+            doWhiteTeamAction();
+            updateGameState();
+        }
+        System.out.printf("Final State: %s%n", gameState);
+    }
+
+    /**
+     * Returns {@code true} if the game is running, {@code false} otherwise.
+     *
+     * @return if the game is running
+     */
+    public boolean isRunning() {
+        return gameState == GameState.RUNNING;
+    }
+
+    /**
+     * Runs the initialization of the white stone.
+     */
+    public void initWhiteStone() {
+        crash(); // TODO: H2.1 - remove if implemented
+    }
+
+    /**
+     * Runs the initialization of all black stones.
+     */
+    public void initBlackStones() {
+        crash(); // TODO: H2.2 - remove if implemented
+    }
+
+    /**
+     * Runs the action of the black team.
+     */
+    public void doBlackTeamAction() {
+        crash(); // TODO: H3.1 - remove if implemented
+    }
+
+    /**
+     * Runs the action of the white team.
+     */
+    public void doWhiteTeamAction() {
+        crash(); // TODO: H3.2 - remove if implemented
+    }
+
+    /**
+     * Checks if a team has won the game and, if so, updates the game state to {@link GameState#BLACK_WIN} or {@link GameState#WHITE_WIN}.
+     */
+    public void updateGameState() {
+        crash(); // TODO: H3.3 - remove if implemented
+    }
+
+    /**
+     * Returns an instance of {@link Random}.
+     *
+     * @return an instance of {@link Random}
+     */
+    private Random getRandom() {
+        return ThreadLocalRandom.current();
+    }
+}
