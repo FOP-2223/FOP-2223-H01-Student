b5f605f33b8132fedff08a2f93e61b12645c627c
Alexander Städing <alexanderstaeding@gmail.com>
2022-10-17 00:18:11 +0200

    Update to Jagr v0.6.0 and add algoutils

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,36 +2,29 @@
 plugins {
     java
     application
-    alias(libs.plugins.submitter)
+    alias(libs.plugins.jagr)
 }
 
-submit {
-    assignmentId = "h_id_" // do not change assignmentId
-    studentId = null // TU-ID  z.B. "ab12cdef"
-    firstName = null
-    lastName = null
-    // Optionally require tests for prepareSubmission task. Default is true
-    requireTests = true
-    // Optionally require public tests for prepareSubmission task. Default is false
-    requirePublicTests = false
-}
-
-// !! Achtung !!
-// Die studentId (TU-ID) ist keine Matrikelnummer
-// Richtig z.B. ab12cdef
-// Falsch z.B. 1234567
-
-repositories {
-    mavenCentral()
-}
-
-val publicTest: SourceSet by sourceSets.creating {
-    val test = sourceSets.test.get()
-    compileClasspath += test.output + test.compileClasspath
-    runtimeClasspath += output + test.runtimeClasspath
+jagr {
+    assignmentId.set("h_id_")
+    submissions {
+        val main by creating {
+// ACHTUNG! Entfernen Sie '//' in den folgenden Zeilen und setzen Sie Ihre TU-ID (NICHT Matrikelnummer!), Vor- und Nachnamen ein.
+//            studentId.set("ab12cdef")
+//            firstName.set("FirstName")
+//            lastName.set("LastName")
+        }
+    }
+    graders {
+        val graderPublic by creating {
+            graderName.set("_name_-Public")
+            rubricProviderName.set("h_id_.H_id__RubricProvider")
+        }
+    }
 }
 
 dependencies {
+    implementation(libs.algoutils.student)
     implementation(libs.annotations)
     // JUnit only available in "test" source set (./src/test)
     testImplementation(libs.junit)
@@ -55,19 +48,6 @@ tasks {
         }
         workingDir = runDir
         useJUnitPlatform()
-    }
-    val publicTest by creating(Test::class) {
-        group = "verification"
-        doFirst {
-            runDir.mkdirs()
-        }
-        workingDir = runDir
-        testClassesDirs = publicTest.output.classesDirs
-        classpath = publicTest.compileClasspath + publicTest.runtimeClasspath
-        useJUnitPlatform()
-    }
-    named("check") {
-        dependsOn(publicTest)
     }
     withType<JavaCompile> {
         options.encoding = "UTF-8"
