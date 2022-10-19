5fd97374178c849d6f4db17771fef2ae82313cdd
Alexander Städing <alexanderstaeding@gmail.com>
2022-10-19 10:04:29 +0200

    Delete incorrect packages and rename grader

--- src/graderPublic/java/h01/H01_RubricProvider.java
+++ src/graderPublic/java/h01/H01_RubricProvider.java
@@ -1,10 +1,8 @@
 package h01;
 
 import org.sourcegrade.jagr.api.rubric.Rubric;
-import org.sourcegrade.jagr.api.rubric.RubricForSubmission;
 import org.sourcegrade.jagr.api.rubric.RubricProvider;
 
-@RubricForSubmission("h01")
 public class H01_RubricProvider implements RubricProvider {
 
     public static final Rubric RUBRIC = Rubric.builder()
